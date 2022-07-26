657723abb34e211db94bea33241acd0c92d66760
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-07-26 16:54:18 +0300

    Update descriptors.py

--- OOP/descriptors.py
+++ OOP/descriptors.py
@@ -81,3 +81,59 @@ p = Point3D(1, 2, 3)
 p.xr = 5
 print(p.xr, p.__dict__, p.z)
 
+
+#######################
+
+
+class ValidateString:
+    def __init__(self, min_length=3, max_length=100):
+        self.min = min_length
+        self.max = max_length
+
+    def validate(self, string):
+        if type(string) == str:
+            if self.min <= len(string) <= self.max:
+                return True
+        return False
+
+
+class StringValue:
+    def __init__(self, validator):
+        self.validator = validator
+
+    def __set_name__(self, owner, name):
+        self.name = "_" + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        if self.validator.validate(value):
+            setattr(instance, self.name, value)
+
+
+class RegisterForm:
+    login = StringValue(validator=ValidateString())
+    password = StringValue(validator=ValidateString())
+    email = StringValue(validator=ValidateString())
+
+    def __init__(self, login, password, email):
+        self.login = login
+        self.password = password
+        self.email = email
+
+    def get_fields(self):
+        return [self.login, self.password, self.email]
+
+    def show(self):
+        print(
+            f"""
+            <form>
+            Логин: {self.login}
+            Пароль: {self.password}
+            Email: {self.email}
+            </form>
+            """)
+
+f = RegisterForm('Loggg', 'password', 'email')
+f.show()
