01ca57e88aa4f7cb0b5187673c6adbb0f00dddca
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-07-29 11:17:09 +0300

    Update __str__,__repr__,__len__,__abs__.py

--- OOP/__str__,__repr__,__len__,__abs__.py
+++ OOP/__str__,__repr__,__len__,__abs__.py
@@ -192,3 +192,42 @@ linked_lst.remove_obj(2)
 linked_lst.add_obj(ObjList("Python ООП"))
 n = len(linked_lst)  # n = 3
 s = linked_lst(1) # s = Balakirev
+
+
+#######
+
+class Complex:
+    def __init__(self, real, img):
+        self.real = real
+        self.img = img
+
+    @property
+    def real(self):
+        return self.__real
+
+    @real.setter
+    def real(self, real):
+        if type(real) not in [int, float]:
+            raise ValueError("Неверный тип данных.")
+        else:
+            self.__real = real
+
+    @property
+    def img(self):
+        return self.__img
+
+    @img.setter
+    def img(self, img):
+        if type(img) not in [int, float]:
+            raise ValueError("Неверный тип данных.")
+        else:
+            self.__img = img
+
+    def __abs__(self):
+        return (self.real**2 + self.img**2) ** 0.5
+
+
+cmp = Complex(7, 8)
+cmp.real = 3
+cmp.img = 4
+c_abs = abs(cmp)
