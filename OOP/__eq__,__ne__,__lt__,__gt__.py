49e04c3bf3160af77db316a5b52584d0d01d10e4
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-02 11:02:49 +0300

    Update __eq__,__ne__,__lt__,__gt__.py

--- OOP/__eq__,__ne__,__lt__,__gt__.py
+++ OOP/__eq__,__ne__,__lt__,__gt__.py
@@ -47,3 +47,52 @@ print(c1 > c2)
 print(c1 < c2)
 print(c1 >= c2)
 print(c1 <= c2)
+
+
+
+#########
+
+
+class Track:
+    def __init__(self, start_x=0, start_y=0):
+        # self.start_x = start_x
+        # self.start_y = start_y
+        self.track = [TrackLine(start_x, start_y)]
+
+    def add_track(self, tr):
+        self.track.append(tr)
+
+    def get_tracks(self):
+        return tuple(self.track)
+
+    def __len__(self):
+        l = 0
+        for i in range(len(self.track)-1):
+            l += ((self.track[i].to_x - self.track[i+1].to_x)**2 + (self.track[i].to_y - self.track[i+1].to_y)**2)**0.5
+
+        return int(l)
+
+    def __eq__(self, other):
+        return len(self) == len(other)
+
+    def __lt__(self, other):
+        return len(self) < len(other)
+
+
+class TrackLine:
+    def __init__(self, to_x, to_y, max_speed=0):
+        self.to_x = to_x
+        self.to_y = to_y
+        self.max_speed = max_speed
+
+
+track1, track2 = Track(), Track(0, 1)
+track1.add_track(TrackLine(2, 4, 100))
+track1.add_track(TrackLine(5, -4, 100))
+track2.add_track(TrackLine(3, 2, 90))
+track2.add_track(TrackLine(10, 8, 90))
+res_eq = track1 == track2
+print(res_eq)
+print(len(track1))
+print(len(track2))
+
