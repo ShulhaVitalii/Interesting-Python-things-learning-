2f3e114d39e0fe7ec84a62b35fe5eddadc920bbd
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-02 13:38:40 +0300

    Update __eq__,__ne__,__lt__,__gt__.py

--- OOP/__eq__,__ne__,__lt__,__gt__.py
+++ OOP/__eq__,__ne__,__lt__,__gt__.py
@@ -205,3 +205,39 @@ lst_text_sorted = [' '.join(i.lst_words) for i in sorted(lst_text, key=lambda x:
 
 print(*lst_text_sorted, sep='\n')
 
+
+
+###########
+
+
+
+class Morph:
+    def __init__(self, *args):
+        self.words = [*args]
+
+    def add_word(self, word):
+        if word not in self.words:
+            self.words.append(word)
+
+    def get_words(self):
+        return tuple(self.words)
+
+    def __eq__(self, other):
+        return other.lower() in self.words
+
+
+dict_words = [Morph('связь', 'связи', 'связью', 'связи', 'связей', 'связям', 'связями', 'связях'),
+              Morph('формула', 'формулы', 'формуле', 'формулу', 'формулой', 'формул', 'формулам', 'формулами', 'формулах'),
+              Morph('вектор', 'вектора', 'вектору', 'вектором', 'векторе', 'векторы', 'векторов', 'векторам',
+                    'векторами', 'векторах'),
+              Morph('эффект', 'эффекта', 'эффекту', 'эффектом', 'эффекте', 'эффекты', 'эффектов', 'эффектам',
+                    'эффектами', 'эффектах'),
+              Morph('день', 'дня', 'дню', 'днем', 'дне', 'дни', 'дням', 'днями', 'днях')]
+
+text = input().strip('.').lower()
+counter = 0
+for i in text.split():
+    for word in dict_words:
+        if word == i:
+            counter += 1
+print(counter)
