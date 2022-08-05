5a4cf4724b91e5eddae0d810d87506aa624b2b0a
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-05 12:14:47 +0300

    Update __getitem____setitem____delitem__.py

--- OOP/__getitem____setitem____delitem__.py
+++ OOP/__getitem____setitem____delitem__.py
@@ -64,3 +64,38 @@ class Record:
             raise IndexError('неверный индекс поля')
             
             
+#####
+
+
+class Track:
+    def __init__(self, start_x, start_y):
+        self.x = start_x
+        self.y = start_y
+        self.road = [[(self.x, self.y), None]]
+
+    def add_point(self, x, y, speed):
+        self.road.append([(x, y), speed])
+
+    def __getitem__(self, item):
+        if 0 <= item < len(self.road)-1:
+            return self.road[item+1][0], self.road[item+1][1]
+        else:
+            raise IndexError('некорректный индекс')
+
+    def __setitem__(self, key, value):
+        if 0 <= key < len(self.road) - 1:
+            if type(value) in (float, int):
+                self.road[key+1][1] = value
+        else:
+            raise IndexError('некорректный индекс')
+
+
+tr = Track(10, -5.4)
+tr.add_point(20, 0, 100)  # первый линейный сегмент: indx = 0
+tr.add_point(50, -20, 80)  # второй линейный сегмент: indx = 1
+tr.add_point(63.45, 1.24, 60.34)  # третий линейный сегмент: indx = 2
+
+tr[2] = 60
+c, s = tr[2]
+print(c, s)  # => (63.45, 1.24) 60
+res = tr[3]  # IndexError
