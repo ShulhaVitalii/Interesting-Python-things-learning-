db0a0b7ef361b31491c104182e3b6250f523a939
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-08 12:02:03 +0300

    Update __getitem____setitem____delitem__.py
    Added TicTacToe

--- OOP/__getitem____setitem____delitem__.py
+++ OOP/__getitem____setitem____delitem__.py
@@ -33,6 +33,90 @@ class Student:
 s1 = Student('Vitalii', [5, 5, 3, 2, 5])
 s1[2] = 4
 print(s1[2])
+
+
+####
+
+
+class TicTacToe:
+    def __init__(self):
+        self.pole = [[Cell() for _ in range(3)]for _ in range(3)]
+
+    def clear(self):
+        self.pole = [[Cell() for _ in range(3)]for _ in range(3)]
+
+    def __getitem__(self, item):
+        if type(item[1]) == int and type(item[0]) == int:
+            return self.pole[item[0]][item[1]].value
+
+        if type(item[1]) == slice:
+            res = self.pole[item[0]][item[1]]
+            return tuple(i.value for i in res)
+
+        if type(item[0]) == slice:
+            pole = [[0 for _ in range(3)] for _ in range(3)]
+            for i in range(3):
+                for j in range(3):
+                    pole[i][j] = self.pole[j][i]
+            return tuple(i.value for i in pole[item[1]])
+
+    def __setitem__(self, key, value):
+        if -1 >= key[0] >= 3 or -1 >= key[1] >= 3:
+            raise IndexError('неверный индекс клетки')
+
+        if bool(self.pole[key[0]][key[1]]):
+            self.pole[key[0]][key[1]].value = value
+            self.pole[key[0]][key[1]].is_free = False
+        else:
+            raise ValueError('клетка уже занята')
+        return self
+
+
+class Cell:
+    def __init__(self):
+        self.is_free = True
+        self.value = 0
+
+    def __bool__(self):
+        return self.is_free
+
+
+g = TicTacToe()
+g.clear()
+assert g[0, 0] == 0 and g[2, 2] == 0, "начальные значения всех клеток должны быть равны 0"
+g[1, 1] = 1
+g[2, 1] = 2
+assert g[1, 1] == 1 and g[
+    2, 1] == 2, "неверно отработала операция присваивания новых значений клеткам игрового поля (или, некорректно работает считывание значений)"
+
+try:
+    res = g[3, 0]
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError при считывании из несуществующей ячейки"
+
+try:
+    g[3, 0] = 5
+except IndexError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение IndexError при записи в несуществующую ячейку"
+
+g.clear()
+g[0, 0] = 1
+g[1, 0] = 2
+g[2, 0] = 3
+
+assert g[0, :] == (1, 0, 0) and g[1, :] == (2, 0, 0) and g[:, 0] == (
+1, 2, 3), "некорректно отработали срезы после вызова метода clear() и присваивания новых значений"
+
+cell = Cell()
+assert cell.value == 0, "начальное значение атрибута value класса Cell должно быть равно 0"
+res = cell.is_free
+cell.is_free = True
+assert bool(cell), "функция bool вернула False для свободной клетки"
+
 del s1[2]
 s1[10] = 5
 print(s1.marks)
