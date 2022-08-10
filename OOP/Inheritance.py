30011ff0a1bf2a69673c14bacca2ff5e2f518794
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-10 11:01:53 +0300

    Update Inheritance.py

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -122,3 +122,49 @@ dv = DetailView(methods=('GET', 'POST'))
 html = dv.render_request({'url': 'https://site.ru/home'}, 'GET')
 print(html)
 
+
+
+###########
+
+
+class Validator:
+    """
+    https://stepik.org/lesson/701995/step/9?thread=solutions&unit=702096
+    """
+    def _is_valid(self, data):
+        return True
+
+    def __call__(self, data, *args, **kwargs):
+        if not self._is_valid(data):
+            raise ValueError('данные не прошли валидацию')
+        return True
+
+
+class IntegerValidator(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def _is_valid(self, data):
+        if type(data) == int and self.min_value <= data <= self.max_value:
+            return True
+        return False
+
+
+class FloatValidator(Validator):
+    def __init__(self, min_value, max_value):
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def _is_valid(self, data):
+        if type(data) == float and self.min_value <= data <= self.max_value:
+            return True
+        return False
+
+v = Validator()
+v(4)
+integer_validator = IntegerValidator(-10, 10)
+float_validator = FloatValidator(-1, 1)
+res1 = integer_validator(10)  # True
+print(res1)
+res2 = float_validator(10)    # исключение ValueError
