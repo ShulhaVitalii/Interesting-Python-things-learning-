25b2b10386f5471306997e22d9f3ffb3db3c5422
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-10 12:51:37 +0300

    Update Inheritance.py

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -225,3 +225,76 @@ for x in NetworkIterator(nt):
     n += 1
 
 assert n == 4, "итератор перебрал неверное число слоев"
+
+
+
+############
+
+
+
+class Vector:
+    def __init__(self, *args):
+        self.coords = [*args]
+
+    def get_coords(self):
+        return tuple(self.coords)
+
+    def __add__(self, other):
+        if len(self.get_coords()) != len(other.get_coords()):
+            raise TypeError('размерности векторов не совпадают')
+        res = []
+        for i in range(len(self.coords)):
+            res.append(self.coords[i] + other.coords[i])
+        if all([type(i) == int for i in res]):
+            return self.__class__(*res)
+        return self.__class__.__base__(*res)
+
+    def __sub__(self, other):
+        if len(self.coords) != len(other.coords):
+            raise TypeError('размерности векторов не совпадают')
+        res = []
+        for i in range(len(self.coords)):
+            res.append(self.coords[i] - other.coords[i])
+        if all([type(i) == int for i in res]):
+            return __class__(*res)
+        return __class__.__base__(*res)
+
+
+class VectorInt(Vector):
+    def __init__(self, *args):
+        if all([type(i) == int for i in args]):
+            super().__init__(*args)
+        else:
+            raise ValueError('координаты должны быть целыми числами')
+
+
+
+
+
+v1 = Vector(1, 2, 3)
+v2 = Vector(3, 4, 5)
+
+assert (v1 + v2).get_coords() == (
+4, 6, 8), "операция сложения дала неверные значения (или некорректно работает метод get_coords)"
+assert (v1 - v2).get_coords() == (
+-2, -2, -2), "операция вычитания дала неверные значения (или некорректно работает метод get_coords)"
+
+v = VectorInt(1, 2, 3, 4)
+assert isinstance(v, Vector), "класс VectorInt должен наследоваться от класса Vector"
+
+try:
+    v = VectorInt(1, 2, 3.4, 4)
+except ValueError:
+    assert True
+else:
+    assert False, "не сгенерировалось исключение ValueError для команды v = VectorInt(1, 2, 3.4, 4)"
+
+v1 = VectorInt(1, 2, 3, 4)
+v2 = VectorInt(4, 2, 3, 4)
+v3 = Vector(1.0, 2, 3, 4)
+
+v = v1 + v2
+assert type(
+    v) == VectorInt, "при сложении вектором с целочисленными координатами должен формироваться объект класса VectorInt"
+v = v1 + v3
+assert type(v) == Vector, "при сложении вектором с вещественными координатами должен формироваться объект класса Vector"
