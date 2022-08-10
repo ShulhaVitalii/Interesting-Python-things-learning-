87555a366ac75c7d5fb567b9b05ba6c14a921af8
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-10 12:16:52 +0300

    Update Inheritance.py

--- OOP/Inheritance.py
+++ OOP/Inheritance.py
@@ -168,3 +168,60 @@ float_validator = FloatValidator(-1, 1)
 res1 = integer_validator(10)  # True
 print(res1)
 res2 = float_validator(10)    # исключение ValueError
+
+
+########
+
+
+"""
+Используя механизм наследования, вам поручено разработать функционал по построению моделей нейронных сетей...
+https://stepik.org/lesson/701995/step/10?unit=702096
+https://youtu.be/I8upOO_ZjqQ
+"""
+
+class Layer:
+    def __init__(self, name='Layer'):
+        self.next_layer = None
+        self.name = name
+
+    def __call__(self, layer, *args, **kwargs):
+        self.next_layer = layer
+        return layer
+
+
+class Input(Layer):
+    def __init__(self, inputs):
+        super().__init__('Input')
+        self.inputs = inputs
+
+
+class Dense(Layer):
+    def __init__(self, inputs, outputs, activation):
+        super().__init__('Dense')
+        self.inputs = inputs
+        self.outputs = outputs
+        self.activation = activation
+
+
+class NetworkIterator:
+    def __init__(self, start):
+        self.start = start
+
+    def __iter__(self):
+        layer = self.start
+        while layer:
+            yield layer
+            layer = layer.next_layer
+
+
+nt = Input(12)
+layer = nt(Dense(nt.inputs, 1024, 'relu'))
+layer = layer(Dense(layer.inputs, 2048, 'relu'))
+layer = layer(Dense(layer.inputs, 10, 'softmax'))
+
+n = 0
+for x in NetworkIterator(nt):
+    assert isinstance(x, Layer), "итератор должен возвращать объекты слоев с базовым классом Layer"
+    n += 1
+
+assert n == 4, "итератор перебрал неверное число слоев"
