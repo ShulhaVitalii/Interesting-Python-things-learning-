a795965d009198ad3ab526a006e9b01adc177117
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-11 15:47:59 +0300

    Update decorators.py

--- decorators.py
+++ decorators.py
@@ -34,3 +34,40 @@ def get_fast_nod(a, b):
 res = get_nod(2, 1000000)
 res1 = get_fast_nod(652623, 107876464)
 print(res1)
+
+
+### Class decorator
+
+
+class Router:
+    app = {}
+
+    @classmethod
+    def get(cls, path):
+        return cls.app.get(path)
+
+    @classmethod
+    def add_callback(cls, path, func):
+        cls.app[path] = func
+
+
+class Callback:
+    def __init__(self, path, router_cls):
+        self.path = path
+        self.router_cls = router_cls
+
+    def __call__(self, func):
+        self.router_cls.add_callback(self.path, func)
+
+
+@Callback('/about', Router)
+def about():
+    return '<h1>About</h1>'
+
+
+route = Router.get('/about')
+ret = route()
+assert ret == '<h1>About</h1>', "декорированная функция вернула неверные данные"
+
+route = Router.get('/')
+assert route is None, "Класс Router, при вызове метода get, вернул неверные данные"
