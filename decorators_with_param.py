c80b8ca2f5f653c0a39a8328cf35a99002657531
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-11 16:27:08 +0300

    Update decorators_with_param.py

--- decorators_with_param.py
+++ decorators_with_param.py
@@ -30,3 +30,51 @@ df = sin_df(math.pi / 3)
 print(df)
 print(sin_df.__name__)
 print(sin_df.__doc__)
+
+
+
+
+
+######
+
+
+def integer_params_decorated(func):
+    def wrapper(self, *args, **kwargs):
+        for i in args:
+            if type(i) != int:
+                raise TypeError("аргументы должны быть целыми числами")
+
+        if not all(type(x) == int for x in kwargs.values()):
+            raise TypeError("аргументы должны быть целыми числами")
+
+        return func(self, *args, **kwargs)
+    return wrapper
+
+
+def integer_params(cls):
+    methods = {k: v for k, v in cls.__dict__.items() if callable(v)}
+    for k, v in methods.items():
+        setattr(cls, k, integer_params_decorated(v))
+
+    return cls
+
+
+@integer_params
+class Vector:
+    def __init__(self, *args):
+        self.__coords = list(args)
+
+    def __getitem__(self, item):
+        return self.__coords[item]
+
+    def __setitem__(self, key, value):
+        self.__coords[key] = value
+
+    def set_coords(self, *coords, reverse=False):
+        c = list(coords)
+        self.__coords = c if not reverse else c[::-1]
+
+
+vector = Vector(1, 2)
+print(vector[1])
+vector[1] = 20.4 # TypeError
