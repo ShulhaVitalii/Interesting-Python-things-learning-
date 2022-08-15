1c5c01e9a1e3c6b26781c308d8836bb7dc0c49c1
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-15 16:15:28 +0300

    Update multi inheritance.py

--- OOP/multi inheritance.py
+++ OOP/multi inheritance.py
@@ -145,3 +145,59 @@ class Book(ShopItem, ShopUserView):
 
 book = Book("Python ООП", "Балакирев", 2022)
 print(book)
+
+
+###########
+
+
+class MoneyOperators:
+    def __add__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money + other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money + other.money)
+
+    def __sub__(self, other):
+        if type(other) in (int, float):
+            return self.__class__(self.money - other)
+
+        if type(self) != type(other):
+            raise TypeError('Разные типы объектов')
+
+        return self.__class__(self.money - other.money)
+
+
+class Money:
+    def __init__(self, value):
+        self.money = value
+
+    @property
+    def money(self):
+        return self._money
+
+    @money.setter
+    def money(self, value):
+        if type(value) not in (int, float):
+            raise TypeError('сумма должна быть числом')
+        self._money = value
+
+
+class MoneyR(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyR: {self.money}"
+
+
+class MoneyD(Money, MoneyOperators):
+    def __str__(self):
+        return f"MoneyD: {self.money}"
+
+
+m1 = MoneyR(1)
+m2 = MoneyD(2)
+m = m1 + 10
+print(m)  # MoneyR: 11
+m = m1 - 5.4
+m = m1 + m2  # TypeError
