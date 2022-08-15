a4d0e29fe1dde579196dd9d12c39afb412d81e3b
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-15 13:22:18 +0300

    added example

--- OOP/multi inheritance.py
+++ OOP/multi inheritance.py
@@ -41,3 +41,61 @@ n.save_sell_log()
 print(NoteBook.__mro__)
 
 MixinLog.print_info(n)
+
+
+
+######
+
+
+class Digit:
+    def __init__(self, value):
+        self.checking_value(value)
+        self.value = value
+
+    def checking_value(self, value):
+        if not isinstance(value, (int, float)):
+            raise TypeError('значение не соответствует типу объекта')
+
+
+class Integer(Digit):
+    def checking_value(self, value):
+        super().checking_value(value)
+        if type(value) != int:
+            raise TypeError('значение не соответствует типу объекта')
+
+
+class Float(Digit):
+    def checking_value(self, value):
+        super().checking_value(value)
+        if type(value) != float:
+            raise TypeError('значение не соответствует типу объекта')
+
+
+class Positive(Digit):
+    def checking_value(self, value):
+        super().checking_value(value)
+        if value <= 0:
+            raise TypeError('значение не соответствует типу объекта')
+
+
+class Negative(Digit):
+    def checking_value(self, value):
+        super().checking_value(value)
+        if value >= 0:
+            raise TypeError('значение не соответствует типу объекта')
+
+
+class PrimeNumber(Integer, Positive):
+    pass
+
+
+class FloatPositive(Float, Positive):
+    pass
+
+
+digits = [PrimeNumber(3), PrimeNumber(1), PrimeNumber(4), FloatPositive(1.5), FloatPositive(9.2), FloatPositive(6.5),
+          FloatPositive(3.5), FloatPositive(8.9)]
+
+lst_positive = [i for i in digits if isinstance(i, Positive)]
+lst_float = [i for i in digits if isinstance(i, Float)]
+
