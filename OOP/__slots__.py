3b094585ee6f64c99269aad5a78b70e5cd7429a6
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-16 14:49:24 +0300

    Update __slots__.py

--- OOP/__slots__.py
+++ OOP/__slots__.py
@@ -117,3 +117,60 @@ class SolarSystem:
 
 s_system = SolarSystem()
 
+
+
+##########
+
+
+class Function:
+    def __init__(self):
+        self._amplitude = 1.0     # амплитуда функции
+        self._bias = 0.0          # смещение функции по оси Oy
+
+    def __call__(self, x, *args, **kwargs):
+        return self._amplitude * self._get_function(x) + self._bias
+
+    def _get_function(self, x):
+        raise NotImplementedError('метод _get_function должен быть переопределен в дочернем классе')
+
+    def __add__(self, other):
+        if type(other) not in (int, float):
+            raise TypeError('смещение должно быть числом')
+
+        obj = self.__class__(self)
+        obj._bias = self._bias + other
+
+        return obj
+
+    def __mul__(self, other):
+        if type(other) not in (int, float):
+            raise TypeError('смещение должно быть числом')
+
+        obj = self.__class__(self)
+        obj._amplitude = self._amplitude * other
+        print(obj.__dict__)
+
+        return obj
+
+
+class Linear(Function):
+    def __init__(self, *args):
+        super().__init__()
+        if len(args) == 2:
+            self._k = args[0]
+            self._b = args[1]
+        elif len(args) == 1:
+            self._k = args[0]._k
+            self._b = args[0]._b
+
+    def _get_function(self, x):
+        return self._k * x + self._b
+
+
+f = Linear(1, 0.5)
+f2 = f * 5    # изменение амплитуды (атрибут _amplitude)
+print(f2._amplitude)
+y1 = f(0)
+print(y1)# 0.5
+y2 = f2(0)
+print(y2)
