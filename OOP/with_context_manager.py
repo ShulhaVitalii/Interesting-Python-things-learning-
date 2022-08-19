cf4fc0e093dd39e779fbef7a415eb3dc2cf07dd7
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-19 10:23:23 +0300

    Create with_context_manager.py

---
+++ OOP/with_context_manager.py
@@ -0,0 +1,33 @@
+"""
+Менеджеры контекстов. Оператор with
+    __enter__
+    __exit__
+"""
+
+class DefenderVector:
+    def __init__(self, v):
+        self.__v = v
+
+    def __enter__(self):
+        self.__temp = self.__v[:]
+        return self.__temp
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if exc_type is None:
+            self.__v[:] = self.__temp
+
+        return False
+
+
+v1 = [1, 2, 3]
+v2 = [2, 3]
+try:
+    with DefenderVector(v1) as dv:
+        for i, a in enumerate(dv):
+            dv[i] += v2[i]
+except:
+    print("Error")
+print(v1)
+
+
+#####
