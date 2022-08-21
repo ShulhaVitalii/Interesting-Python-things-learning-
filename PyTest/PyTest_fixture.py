f2b674162489970893db8ae948e2724225725364
ShulhaVitalii <107625988+ShulhaVitalii@users.noreply.github.com>
2022-08-21 17:37:54 +0300

    Update PyTest_fixture.py

--- PyTest/PyTest_fixture.py
+++ PyTest/PyTest_fixture.py
@@ -21,4 +21,20 @@ def append_third(order, append_second):
 
 def test_order(order):
     assert order == [1, 2, 3]
+ 
+
+
+
+
+@pytest.fixture
+def smtp_connection():
+    import smtplib
+
+    return smtplib.SMTP("smtp.gmail.com", 587, timeout=5)
+
+
+def test_ehlo(smtp_connection):
+    response, msg = smtp_connection.ehlo()
+    assert response == 250
+    assert 0  # в демонстрационных целях
     
